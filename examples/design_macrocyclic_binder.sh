070c2a89970a8c7989fec17e3a842a7877be3547
codergirl1106 <68753302+codergirl1106@users.noreply.github.com>
2025-12-23 10:28:57 -0500

    Update design_macrocyclic_binder.sh

--- examples/design_macrocyclic_binder.sh
+++ examples/design_macrocyclic_binder.sh
@@ -4,10 +4,10 @@ prefix=./outputs/diffused_binder_cyclic2
 
 # Note that the indices in this pdb file have been 
 # shifted by +2 in chain A relative to pdbID 7zkr.
-pdb='./input_pdbs/7zkr_GABARAP.pdb'
+pdb='./rdf_macro/examples/input_pdbs/7zkr_GABARAP.pdb'
 
 num_designs=10
-script="../scripts/run_inference.py"
+script="./rdf_macro/run_inference.py"
 $script --config-name base \
 inference.output_prefix=$prefix \
 inference.num_designs=$num_designs \
